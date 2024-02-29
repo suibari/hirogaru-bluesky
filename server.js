a7acb0e89584c146ce3e92903a9748b9956c4662
suibari <404.not.found.address@gmail.com>
2024-02-29 11:32:10 +0900

    tmp commit. cytoscape版に修正.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const MyBskyAgent = require('./src/bluesky');
 const databuilder = require('./src/databuilder');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -21,54 +22,66 @@ app.listen(port, () => {
 
 async function getData() {
   const FF_THRESHOLD = 1000;
+  const NODE_LIMIT = 5000;
   const handleordid = "suibari-cha.bsky.social";
 
   const agent = new MyBskyAgent();
 
   try {
-  await agent.login({
-    identifier: process.env.BSKY_IDENTIFIER,
-    password: process.env.BSKY_APP_PASSWORD
-  });
-  // 1st depth
-  const response = await agent.getProfile({actor: handleordid});
-  const follows = await agent.getConcatFollows(handleordid);
-  const followers = await agent.getConcatFollowers(handleordid);
-  let data = databuilder.getNodesAndLinks(response.data, follows, followers);
-  console.log("complete myself");
-  // 2nd~ depth
-  for (const follow of follows) {
-    const response = await agent.getProfile({actor: follow.did});
-    // console.log(response.data)
-    if ((response.data.followsCount < FF_THRESHOLD) && (response.data.followersCount < FF_THRESHOLD)) {
-      console.log("start follow: "+follow.displayName);
-      // follow
-      const followOfFollows = await agent.getConcatFollows(follow.did);
-      const followOfFollowers = await agent.getConcatFollowers(follow.did);
-      const dataOfFollows = databuilder.getNodesAndLinks(follow, followOfFollows, followOfFollowers);
-      data.nodes = data.nodes.concat(dataOfFollows.nodes);
-      data.links = data.links.concat(dataOfFollows.links);
-      console.log("complete follow: "+follow.displayName);
-    }
-  };
-  for (const follower of followers) {
-    const response = await agent.getProfile({actor: follower.did});
-    if ((response.data.followsCount < FF_THRESHOLD) && (response.data.followersCount < FF_THRESHOLD)) {
-      console.log("start follower: "+follower.displayName);
-      // follower
-      const followerOfFollows = await agent.getConcatFollows(follower.did);
-      const followerOfFollowers = await agent.getConcatFollowers(follower.did);
-      const dataOfFollowers = databuilder.getNodesAndLinks(follower, followerOfFollows, followerOfFollowers);
-      data.nodes = data.nodes.concat(dataOfFollowers.nodes);
-      data.links = data.links.concat(dataOfFollowers.links);
-      console.log("complete follow: "+follower.displayName);
-    }
-  };
-  console.log(data);
-  databuilder.removeInvalidLinks(data);
+    await agent.login({
+      identifier: process.env.BSKY_IDENTIFIER,
+      password: process.env.BSKY_APP_PASSWORD
+    });
+    // 1st depth
+    const response = await agent.getProfile({actor: handleordid});
+    const follows = await agent.getConcatFollows(handleordid);
+    const followers = await agent.getConcatFollowers(handleordid);
+    let elements = await databuilder.getNodesAndLinks(response.data, follows, followers);
+    console.log("complete myself");
+    // 2nd~ depth
+    for (const follow of follows) {
+      const response = await agent.getProfile({actor: follow.did});
+      // console.log(response.data)
+      if ((response.data.followsCount < FF_THRESHOLD) && (response.data.followersCount < FF_THRESHOLD)) {
+        console.log("start follow: "+follow.displayName);
+        // follow
+        const followOfFollows = await agent.getConcatFollows(follow.did);
+        const followOfFollowers = await agent.getConcatFollowers(follow.did);
+        const dataOfFollows = await databuilder.getNodesAndLinks(follow, followOfFollows, followOfFollowers);
+        elements = elements.concat(dataOfFollows);
+        console.log("complete follow: "+follow.displayName);
+      }
+    };
+    for (const follower of followers) {
+      const response = await agent.getProfile({actor: follower.did});
+      if ((response.data.followsCount < FF_THRESHOLD) && (response.data.followersCount < FF_THRESHOLD)) {
+        console.log("start follower: "+follower.displayName);
+        // follower
+        const followerOfFollows = await agent.getConcatFollows(follower.did);
+        const followerOfFollowers = await agent.getConcatFollowers(follower.did);
+        const dataOfFollowers = await databuilder.getNodesAndLinks(follower, followerOfFollows, followerOfFollowers);
+        elements = elements.concat(dataOfFollowers);
+        console.log("complete follow: "+follower.displayName);
+      }
+    };
+    console.log(elements.length);
+    
+    // ファイル保存
+    // for (const element of elements) {
+    //   // data.img プロパティが undefined かどうかをチェックし、空文字列に置き換える
+    //   if (element.data && typeof element.data.img === 'undefined') {
+    //       element.data.img = ''; // 空文字列に置き換える
+    //   }
+    // }
+    // fs.writeFileSync(("data.json", JSON.stringify(elements, null, '    ')));
 
-  console.log(data);
-  return data;
+    elements = databuilder.removeDuplicateAndLimitNodes(elements, NODE_LIMIT);
+    console.log(elements.length);
+
+    databuilder.removeInvalidLinks(elements);
+    console.log(elements.length);
+    
+    return elements;
 
   } catch(e) {
     console.error(e);
--- src/databuilder.js
+++ src/databuilder.js
@@ -1,48 +1,109 @@
-function getNodesAndLinks(myself, follows, followers) {
-  let data = {};
+async function getNodesAndLinks(myself, follows, followers) {
+  let elements = [];
 
   // nodes
-  let nodes = [];
   const personArray = [myself].concat(follows, followers);
   for (const person of personArray) {
-    nodes.push({
-      id: person.did,
-      name: person.displayName,
-      img: person.avatar
+    elements.push({
+      data: {
+        id: person.did,
+        name: person.displayName,
+        img: person.avatar
+      },
+      group: 'nodes'
     });
   };
   console.log("complete nodes");
-  data.nodes = nodes;
   // links
   // * follow (myself -> follow)
-  let links = [];
   for (const follow of follows) {
-    links.push({
-      source: myself.did,
-      target: follow.did
+    elements.push({
+      data: {
+        // id: myself.did+follow.did,
+        source: myself.did,
+        target: follow.did
+      },
+      group: 'edges'
     });
   };
   console.log("complete links: follow");
   // * followers (followers -> myself)
   for (const follower of followers) {
-    links.push({
-      source: follower.did,
-      target: myself.did
-    })
+    elements.push({
+      data: {
+        // id: follower.did+myself.did,
+        source: follower.did,
+        target: myself.did
+      },
+      group: 'edges'
+    });
   }
-  data.links = links;
   console.log("complete links: follower");
 
-  return data;
+  return elements;
 }
 
-function removeInvalidLinks(data) {
-  const validLinks = data.links.filter(link => {
-    const sourceExists = data.nodes.some(node => node.id === link.source);
-    const targetExists = data.nodes.some(node => node.id === link.target);
-    return sourceExists && targetExists;
+// 相互フォローのクラス付け
+// function setClassMutualFF() {
+
+// }
+
+function removeInvalidLinks(elements) {
+  const validEdges = []; // 有効なエッジを格納する配列
+  const validNodes = []; // 有効なノードを格納する配列
+
+  // 有効なエッジとノードを抽出する
+  elements.forEach(element => {
+    if (element.group === 'edges' && element.data && element.data.source && element.data.target) {
+      const sourceExists = elements.some(node => node.group === 'nodes' && node.data.id === element.data.source);
+      const targetExists = elements.some(node => node.group === 'nodes' && node.data.id === element.data.target);
+      if (sourceExists && targetExists) {
+        validEdges.push(element); // 有効なエッジを配列に追加
+      }
+    } else if (element.group === 'nodes') {
+      validNodes.push(element); // 有効なノードを配列に追加
+    }
   });
-  data.links = validLinks;
+
+  // 元の配列を有効なエッジとノードの配列で置き換える
+  elements.length = 0;
+  elements.push(...validEdges, ...validNodes);
+}
+
+function removeDuplicateAndLimitNodes(elements, NODE_LIMIT) {
+  const nodeCounts = {}; // ノードの出現回数を管理するオブジェクト
+  let totalNodeCount = 0; // 全ノードの累計数
+
+  // ノードの重複を削除
+  const filteredNodes = elements.filter(element => {
+    if (element.group === 'nodes') {
+      const nodeId = element.data.id;
+      if (!nodeCounts[nodeId]) {
+        nodeCounts[nodeId] = 0;
+      }
+      nodeCounts[nodeId]++;
+      totalNodeCount++;
+      return nodeCounts[nodeId] === 1; // 重複していないノードのみを残す
+    }
+    return true; // エッジの場合はフィルタしない
+  });
+
+  // 全ノードの累計がNODE_LIMITを超えた場合は超過分を削除する
+  if (totalNodeCount > NODE_LIMIT) {
+    const excess = totalNodeCount - NODE_LIMIT; // 超過分の数
+    let removedCount = 0; // 削除したノードの数
+    for (let i = filteredNodes.length - 1; i >= 0; i--) {
+      if (filteredNodes[i].group === 'nodes') {
+        filteredNodes.splice(i, 1);
+        removedCount++;
+        if (removedCount === excess) {
+          break; // 超過分をすべて削除したらループを終了
+        }
+      }
+    }
+  }
+
+  return filteredNodes;
 }
 
 // const testData = {
@@ -66,3 +127,4 @@ function removeInvalidLinks(data) {
 
 module.exports.getNodesAndLinks = getNodesAndLinks;
 module.exports.removeInvalidLinks = removeInvalidLinks;
+module.exports.removeDuplicateAndLimitNodes = removeDuplicateAndLimitNodes;
