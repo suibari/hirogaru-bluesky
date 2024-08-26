7013b1c88df314931a8975b73fa3db3847ced31f
suibari <404.not.found.address@gmail.com>
2024-08-27 00:44:26 +0900

    vercel用に修正

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,9 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
 };
-
-export default config;
